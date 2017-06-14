21ab19ee9a1591200308647eb070e8c005c0baef
TheBong <robert.urban@outlook.de>
2017-06-14 15:39:40 +0200

    init repo with boilerplate as reference point

---
+++ client/app/app.module.ts
@@ -0,0 +1,47 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+import { RoutingModule } from './routing.module';
+import { SharedModule } from './shared/shared.module';
+import { CatService } from './services/cat.service';
+import { UserService } from './services/user.service';
+import { AuthService } from './services/auth.service';
+import { AuthGuardLogin } from './services/auth-guard-login.service';
+import { AuthGuardAdmin } from './services/auth-guard-admin.service';
+import { AppComponent } from './app.component';
+import { CatsComponent } from './cats/cats.component';
+import { AboutComponent } from './about/about.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+import { AccountComponent } from './account/account.component';
+import { AdminComponent } from './admin/admin.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CatsComponent,
+    AboutComponent,
+    RegisterComponent,
+    LoginComponent,
+    LogoutComponent,
+    AccountComponent,
+    AdminComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    RoutingModule,
+    SharedModule
+  ],
+  providers: [
+    AuthService,
+    AuthGuardLogin,
+    AuthGuardAdmin,
+    CatService,
+    UserService
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule { }
---
+++ server/routes.ts
@@ -0,0 +1,35 @@
+import * as express from 'express';
+
+import CatCtrl from './controllers/cat';
+import UserCtrl from './controllers/user';
+import Cat from './models/cat';
+import User from './models/user';
+
+export default function setRoutes(app) {
+
+  const router = express.Router();
+
+  const catCtrl = new CatCtrl();
+  const userCtrl = new UserCtrl();
+
+  // Cats
+  router.route('/cats').get(catCtrl.getAll);
+  router.route('/cats/count').get(catCtrl.count);
+  router.route('/cat').post(catCtrl.insert);
+  router.route('/cat/:id').get(catCtrl.get);
+  router.route('/cat/:id').put(catCtrl.update);
+  router.route('/cat/:id').delete(catCtrl.delete);
+
+  // Users
+  router.route('/login').post(userCtrl.login);
+  router.route('/users').get(userCtrl.getAll);
+  router.route('/users/count').get(userCtrl.count);
+  router.route('/user').post(userCtrl.insert);
+  router.route('/user/:id').get(userCtrl.get);
+  router.route('/user/:id').put(userCtrl.update);
+  router.route('/user/:id').delete(userCtrl.delete);
+
+  // Apply the routes to our application with the prefix /api
+  app.use('/api', router);
+
+}
