daa4cf531a278470d1f16a54ee0c52d115587719
arubacao <arubacao@gmail.com>
2017-06-18 17:54:59 +0200

    populate `categories` during login in jwt

--- server/controllers/user.ts
+++ server/controllers/user.ts
@@ -9,14 +9,20 @@ export default class UserCtrl extends BaseCtrl {
 
   login = (req, res) => {
     console.log("Login (UserCtrl): " + req.headers);
-    this.model.findOne({ email: req.body.email }, (err, user) => {
-      if (!user) { return res.sendStatus(403); }
-      user.comparePassword(req.body.password, (error, isMatch) => {
-        if (!isMatch) { return res.sendStatus(403); }
-        const token = jwt.sign({ user: user }, process.env.SECRET_TOKEN); // , { expiresIn: 10 } seconds
-        res.status(200).json({ token: token });
+    this.model.findOne({email: req.body.email})
+      .populate('categories')
+      .exec(function (err, user) {
+        if (!user) {
+          return res.sendStatus(403);
+        }
+        user.comparePassword(req.body.password, (error, isMatch) => {
+          if (!isMatch) {
+            return res.sendStatus(403);
+          }
+          const token = jwt.sign({user: user}, process.env.SECRET_TOKEN); // , { expiresIn: 10 } seconds
+          res.status(200).json({token: token});
+        });
       });
-    });
   };
 
   getAll = (req, res) => {
