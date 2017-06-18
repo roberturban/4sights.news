5908b58b88ad2c8842343cc178b559d849b59fd5
Kevin Falkenstein <ga25tuk@mytum.de>
2017-06-18 10:24:49 +0200

    Add backend authentication for deleting topics. Disable "Edit" and "delete" buttons on frontend

--- server/controllers/topic.ts
+++ server/controllers/topic.ts
@@ -100,4 +100,61 @@ export default class TopicCtrl extends BaseCtrl {
       });
     });
   };
+
+  delete = (req, res) => {
+    const token = req.headers.authorization.split(" ")[1]; //first part of string is "Bearer "
+    const model = this.model; //cannot access model otherwise in the following callback
+    console.log("Delete topic");
+    jwt.verify(token, process.env.SECRET_TOKEN, function(err, decoded) {
+      if(err) {
+        console.log("Delete topic - verification failed");
+        return res.status(401).json({
+          title: 'Not Authenticated',
+          error: err
+        });
+      }
+
+      if(decoded.user.role != 'admin') {
+        console.log("Delete topic - unvalid role");
+        return res.status(405).json({
+          title: 'Not Allowed',
+          error: {message: 'Not allowed'}
+        });
+      }
+      
+      model.findById(req.params.id, function(err, topic) {
+        console.log("Delete topic - find topic");
+        if (err) {
+          console.log("Update topic - find topic problem");
+          return res.status(500).json({
+              title: 'An error occurred',
+              error: err
+          });
+        }
+        if (!topic) {
+          console.log("Delete topic - no topic found");
+          return res.status(500).json({
+              title: 'Topic not found!',
+              error: {message: 'Topic not found'}
+          });
+        }
+
+        console.log("Delete topic - delete topic");
+        topic.remove(function (err, result) {
+            if (err) {
+              console.log("Delete topic - delete topic error");
+              return res.status(500).json({
+                  title: 'An error occurred',
+                  error: err
+              });
+            }
+            console.log("Delete topic - success");
+            res.status(200).json({
+                message: 'Topic deleted',
+                obj: result
+            });
+        });
+      });
+    });
+  };
 }
