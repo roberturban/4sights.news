14de75f9daec786851733ad0e01834b97c03bee6
arubacao <arubacao@gmail.com>
2017-06-28 12:47:53 +0200

    add user `isSelf` and `isSelfOrAdmin` middleware

---
+++ server/middleware/user.ts
@@ -0,0 +1,25 @@
+/**
+ * Created by Christopher on 28.06.2017.
+ */
+const user = {
+  isSelf: (req, res, next) => {
+    if (req.payload.user._id !== req.params.id) {
+      return res.status(405).json({
+        title: 'Not Allowed',
+        error: {message: 'Not allowed'}
+      });
+    }
+    next();
+  },
+  isSelfOrAdmin: (req, res, next) => {
+    if (req.payload.user._id === req.params.id || req.payload.user.role === 'admin') {
+      next();
+    }
+    return res.status(405).json({
+      title: 'Not Allowed',
+      error: {message: 'Not allowed'}
+    });
+  }
+};
+
+module.exports = user;
