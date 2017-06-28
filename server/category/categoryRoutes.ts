48924303fc9040965f7bc2f7e4e090382763b296
arubacao <arubacao@gmail.com>
2017-06-28 12:26:56 +0200

    implement Auth & isAdmin logic in category component

--- server/category/categoryRoutes.ts
+++ server/category/categoryRoutes.ts
@@ -6,13 +6,15 @@ import CategoryCtrl from './categoryController';
 const express = require('express');
 const router = express.Router();
 const categoryCtrl = new CategoryCtrl();
+const auth = require('../middleware/auth');
+const admin = require('../middleware/admin');
 
 router.route('/').get(categoryCtrl.getAll);
-router.route('/').post(categoryCtrl.insert);
+router.route('/').post(auth.required, admin.isAdmin, categoryCtrl.insert);
 
 router.route('/:id').get(categoryCtrl.get);
-router.route('/:id').put(categoryCtrl.update);
-router.route('/:id').delete(categoryCtrl.delete);
+router.route('/:id').put(auth.required, admin.isAdmin, categoryCtrl.update);
+router.route('/:id').delete(auth.required, admin.isAdmin, categoryCtrl.delete);
 
 router.route('/count').get(categoryCtrl.count);
 
