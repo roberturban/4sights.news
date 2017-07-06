38eb5b5c9aca4bdb79d27afa98e3e5e1e3375af1
Kevin Falkenstein <ga25tuk@mytum.de>
2017-07-06 17:54:31 +0200

    Fix scraper

--- server/scraper.ts
+++ server/scraper.ts
@@ -1,23 +1,33 @@
 import * as https from 'https';
 import Article from './article/articleModel';
 import Source from './source/sourceModel';
+import * as mongoose from 'mongoose';
+const util = require('util');
 
 module.exports = {
-    startTimedScraping: startTimedScraping,
-    test: test,
-    requestAll: requestAll
+  startTimedScraping: startTimedScraping,
+  test: test,
+  requestAll: requestAll
 };
 
-console.log("Start scraper")
-startTimedScraping();
+mongoose.connect("mongodb://localhost:27017/team26");
+const db = mongoose.connection;
+(<any>mongoose).Promise = global.Promise;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+  console.log("Start scraper")
+  startTimedScraping();
+});
 
 const apikey = "11358ca80a144ea79d32c7879dd4332c"
 
 export function startTimedScraping() {
     console.log("Scraper Interval set")
     setInterval(function() {
-        console.log("Test");
-        test();
+        console.log("<<<<<<<requestAll>>>>>>>>");
+        requestAll();
     }, 1000 * 30);
 }
 
@@ -25,40 +35,31 @@ export function test() {
     console.log("Test function called");
 
     const aljazeeraGET = "/v1/articles?source=al-jazeera-english&sortBy=top&apiKey=" + apikey;
-    const aljazeeraObj = Source.findOne({name: "Aljazeera"});
 
-    requestSource(aljazeeraGET, aljazeeraObj);
+    requestSource(aljazeeraGET, "Aljazeera");
 }
 
 export function requestAll() {
     const aljazeeraGET = "/v1/articles?source=al-jazeera-english&sortBy=top&apiKey=" + apikey;
-    const aljazeeraObj = Source.findOne({name: "Aljazeera"});
     const breitbartGET = "/v1/articles?source=breitbart-news&sortBy=top&apiKey=" + apikey;
-    const breitbartObj = Source.findOne({name: "Breitbart"});
     const cnnGET = "/v1/articles?source=cnn&sortBy=top&apiKey=" + apikey;
-    const cnnObj = Source.findOne({name: "CNN"});
     const bbcGET = "/v1/articles?source=bbc-news&sortBy=top&apiKey=" + apikey;
-    const bbcObj = Source.findOne({name: "BBC"});
     const timesIndiaGET = "/v1/articles?source=the-times-of-india&sortBy=top&apiKey=" + apikey;
-    const timesIndiaObj = Source.findOne({name: "The Times of India"});
     const hinduGET = "/v1/articles?source=the-hindu&sortBy=top&apiKey=" + apikey;
-    const hinduObj = Source.findOne({name: "The Hindu"});
     const nytGET = "/v1/articles?source=the-new-york-times&sortBy=top&apiKey=" + apikey;
-    const nytObj = Source.findOne({name: "The New York Times"});
     const washingtonPostGET = "/v1/articles?source=the-washington-post&sortBy=top&apiKey=" + apikey;
-    const washingtonPostObj = Source.findOne({name: "The Washington Post"});
 
-    requestSource(aljazeeraGET, aljazeeraObj);
-    requestSource(breitbartGET, breitbartObj);
-    requestSource(cnnGET, cnnObj);
-    requestSource(bbcGET, bbcObj);
-    requestSource(timesIndiaGET, timesIndiaObj);
-    requestSource(hinduGET, hinduObj);
-    requestSource(nytGET, nytObj);
-    requestSource(washingtonPostGET, washingtonPostObj);
+    requestSource(aljazeeraGET, "Aljazeera");
+    requestSource(breitbartGET, "Breitbart");
+    requestSource(cnnGET, "CNN");
+    requestSource(bbcGET, "BBC");
+    requestSource(timesIndiaGET, "Times of India");
+    requestSource(hinduGET, "The Hindu");
+    requestSource(nytGET, "The New York Times");
+    requestSource(washingtonPostGET, "Washington Post");
 }
 
-function requestSource(param, sourceObj) {
+function requestSource(param, sourceName) {
     var options = {
         host: "newsapi.org",
         port: 443,
@@ -71,18 +72,17 @@ function requestSource(param, sourceObj) {
 
     const req = https.request(options, function(res) {
         var output = '';
-        console.log('STATUS: ' + res.statusCode);
-        console.log('HEADERS: ' + JSON.stringify(res.headers));
+        //console.log('STATUS: ' + res.statusCode);
+        //console.log('HEADERS: ' + JSON.stringify(res.headers));
         res.setEncoding('utf8');
         res.on('data', function (chunk) {
             output += chunk;
         });
 
         res.on('end', function() {
-            console.log(output)
+            //console.log(output)
             var obj = JSON.parse(output);
-            console.log("Done: " + obj);
-            saveData(obj, sourceObj);
+            saveData(obj, sourceName);
         });
     });
 
@@ -93,39 +93,46 @@ function requestSource(param, sourceObj) {
     req.end();
 }
 
-function saveData(responseObj, sourceObj) {
-    var articles = responseObj["articles"];
-    console.log(articles);
-    for(let index in articles) {
-        const article = articles[index];
+function saveData(responseObj, sourceName) {
+    Source.findOne({"name": sourceName}, function(err, source) {
+        if(err) return console.log("Coule not store article, source " + sourceName + " not found");
 
-        const title = article["title"];
-        const author = article["author"];
-        const description = article["description"];
-        const url = article["url"];
-        const urlToImage = article["urlToImage"];
-        const publishedAt = article["publishedAt"];
+        var articles = responseObj["articles"];
+        for(let index in articles) {
+            const article = articles[index];
+            const title = article["title"];
+            const author = article["author"];
+            const description = article["description"];
+            const url = article["url"];
+            const urlToImage = article["urlToImage"];
+            const publishedAt = article["publishedAt"];
 
-        var instance = new Article({
-            title: title,
-            description: description,
-            image: urlToImage,
-            url: url,
-            author: author,
-            published: publishedAt,
-            categories: [],
-            //source: sourceObj
-        });
+            var instance = new Article({
+                title: title,
+                description: description,
+                image: urlToImage,
+                url: url,
+                author: author,
+                published: publishedAt,
+                categories: [],
+                source: source
+            });
 
-        instance.save((err, item) => {
-            // 11000 is the code for duplicate key error
-            if (err && err.code === 11000) {
-                console.error("Item duplicated");
-            }
-            if (err) {
-                console.error(err);
-            }
-            console.log("Item saved: " + item);
-        });
-    }
+            Article.count({"url": url}, function(err, count){
+                if(err) return console.log("Error counting the number of existing articles");
+                if(count > 0) return console.log("Already stored (" + sourceName + "): \"" + title + "\"");
+
+                instance.save((err, item) => {
+                    // 11000 is the code for duplicate key error
+                    if (err && err.code === 11000) {
+                        console.error("Item duplicated");
+                    }
+                    if (err) {
+                        console.error(err);
+                    }
+                    console.log("Item saved (" + sourceName + "): \"" + title + "\"");
+                });
+            });
+        }
+    });
 }
