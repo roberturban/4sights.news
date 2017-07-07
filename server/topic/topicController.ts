80e216e4b3f2caa85dace455463967c045e6acfe
arubacao <arubacao@gmail.com>
2017-07-07 15:32:53 +0200

    deep population for sources within articles within topic

--- server/topic/topicController.ts
+++ server/topic/topicController.ts
@@ -5,6 +5,10 @@ import Topic from './topicModel';
 import BaseCtrl from '../baseController';
 const param = require('../middleware/param');
 
+// models must be required at least once during request lifecycle
+require('../source/sourceModel');
+require('../article/articleModel');
+
 export default class TopicCtrl extends BaseCtrl {
   model = Topic;
 
@@ -27,7 +31,12 @@ export default class TopicCtrl extends BaseCtrl {
   get = (req, res) => {
     this.model.findOne({_id: req.params.id})
       .populate('categories')
-      .populate('news_articles')
+      .populate({
+        path: 'news_articles',
+        populate: [{
+          path: 'source'
+        }]
+      })
       .exec(function (err, docs) {
         if (err) { return console.error(err); }
         res.json(docs);
