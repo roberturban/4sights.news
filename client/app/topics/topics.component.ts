261bbd1f115302be6099477daa8e2d3aadc2ea5e
arubacao <arubacao@gmail.com>
2017-07-15 18:23:48 +0200

    add category scoping to landing page

--- client/app/topics/topics.component.ts
+++ client/app/topics/topics.component.ts
@@ -180,6 +180,7 @@ export class TopicsComponent implements OnInit {
     if(value=='Home'){
       this.setInitialPage();
     } else{
+      this.getTopics(value);
       this.userCategoryPreferences = [value];
     }
     this.calculateLastRowItems();
